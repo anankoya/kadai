fd9fd8df3b40616c904047a50fbfcf92e42e48c5
anankoya <lt1115fx@ed.ritsumei.ac.jp>
2019-10-23 05:16:02 +0000

    some fix

--- kadai1/index.js
+++ kadai1/index.js
@@ -1,1 +1,14 @@
-console.log(`hello`)
+const firstname = 'taro'
+const secondname = 'yamada'
+console.log(firstname + secondname)
+
+const sum =1+2+3+4+5
+console.log(sum)
+
+const hasdog = false
+const hascat = true
+
+console.log(hasdog || hascat)
+console.log(hasdog && hascat)
+//犬も猫も飼っていない
+console.log(!hasdog && !hascat)
