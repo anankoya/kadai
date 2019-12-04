b1b44aeffbd3bb30022d9f4e8870c0ac2b554e42
anankoya <lt1115fx@ed.ritsumei.ac.jp>
2019-12-04 04:33:39 +0000

    HTMLを追加

---
+++ kadai2/index.js
@@ -0,0 +1,13 @@
+const firstname='taro'
+const secondname='sanada'
+console.log(firdtname+secondname)
+
+const sum=1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const hasdog=false
+const hascat=true
+console.log(hasdog|| hascat)
+console.log(hasdog&&hascat)
+//犬も猫も飼っていない
+console.log(!hasdog&&!hascat)
