8456adaddf86a382187ca28492380cefb5c72739
anankoya <lt1115fx@ed.ritsumei.ac.jp>
2019-12-11 04:28:17 +0000

    HTMLを追加

--- kadai6/index2.js
+++ kadai6/index2.js
@@ -5,6 +5,7 @@ const str4 ='{"a": undefined}'
 const str5 ='{"a":123}'
 const str6 ='{a: 123}'
 
+//str1のバース結果
 try{
     console.log(JSON.parse(str1))
 }catch (error){
@@ -13,6 +14,21 @@ try{
     //正しくは"abc"
 }
 
+//str2のバース結果
+try{
+    console.log(JSON.parse(str2))
+}catch(error){
+    console.log(error)
+    //'abc'という文字列は正しいJSONではないから
+    //正しくは"abc"
+}
 
 
-console.log(JSON.parse(str3))
+//str3のバース結果
+try{
+    console.log(JSON.parse(str3))
+}catch(error){
+    console.log(error)
+    //"abc"は正しいJSONなのでバースできる
+}
+//..str6まで試してみる
